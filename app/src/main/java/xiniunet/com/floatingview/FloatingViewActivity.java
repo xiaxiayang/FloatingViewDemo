6680a0bc7f9e7470547a810ad381e84e2137a227
杨霞 <yangxia@xiniu.com>
2019-02-12 15:20:41 +0800

    功能代码提交

--- app/src/main/java/xiniunet/com/floatingview/FloatingViewActivity.java
+++ app/src/main/java/xiniunet/com/floatingview/FloatingViewActivity.java
@@ -1,12 +1,22 @@
 package xiniunet.com.floatingview;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import xiniunet.com.floatingview.base.BaseActivity;
+import xiniunet.com.floatingview.service.FloatingService;
 
 /**
  * @author yangxia
@@ -14,6 +24,10 @@ import xiniunet.com.floatingview.base.BaseActivity;
  */
 public class FloatingViewActivity extends BaseActivity {
 
+    /**
+     * 悬浮框权限设置
+     */
+    private static final  int PERMISSION_REQUEST_CODE = 0;
     private Chronometer chronometer;
     private ImageView ivMinmize;
 
@@ -38,7 +52,7 @@ public class FloatingViewActivity extends BaseActivity {
         ivMinmize.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveTaskToBack(true);
+                permissonCheck();
             }
         });
 
@@ -50,5 +64,68 @@ public class FloatingViewActivity extends BaseActivity {
         //开启定时器
         chronometer.setBase(0L);
         chronometer.start();
+    }
+
+    /**
+     * 开启悬浮窗权限检查
+     */
+    public void permissonCheck(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //判断系统版本
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT);
+                Intent intent =new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                this.startActivityForResult(intent, PERMISSION_REQUEST_CODE);
+            } else {
+                createFloatingView();
+            }
+        } else {
+            createFloatingView();
+        }
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (mServiceConnection !=null){
+            unbindService(mServiceConnection);
+            mServiceConnection = null;
+        }
+    }
+
+    private ServiceConnection mServiceConnection = null;
+    public void createFloatingView(){
+        moveTaskToBack(true);
+        mServiceConnection  =   new ServiceConnection() {
+
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                // 获取服务的操作对象
+                FloatingService.FloatingBinder binder = (FloatingService.FloatingBinder) service;
+                binder.getService();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+            }
+        };
+       Intent  intentService = new Intent(this, FloatingService.class);
+       intentService.putExtra("time",chronometer.getBase());
+       bindService(intentService, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PERMISSION_REQUEST_CODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                //授权成功后开启悬浮窗
+                createFloatingView();
+            }
+        }
     }
 }
--- app/src/main/java/xiniunet/com/floatingview/service/FloatingService.java
+++ app/src/main/java/xiniunet/com/floatingview/service/FloatingService.java
@@ -9,36 +9,60 @@ import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Chronometer;
-import android.widget.TextView;
+
+import xiniunet.com.floatingview.FloatingViewActivity;
+import xiniunet.com.floatingview.R;
 
 /**
  * @author yangxia
- * @since 21/1/19 下午1:30
- * 语音聊天 窗口最小化处理
+ * @since 11/2/19 下午1:30
+ * 窗口最小化处理
  */
 public class FloatingService extends Service {
-
-    public static boolean isStarted = false;
 
     private WindowManager windowManager;
     private WindowManager.LayoutParams layoutParams;
 
     private View view;
-    private TextView tvStatus;
     private Chronometer chronometer;
-    /**
-     * 更新悬浮框上的文字
-     */
-    public static final String UPDATE_TEXT_FLAG  = "updateTextFlag";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        isStarted = true;
+        initWindowManger();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (windowManager !=null && view !=null){
+            windowManager.removeView(view);
+        }
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        addView(intent);
+        return new FloatingBinder();
+    }
+
+    public class FloatingBinder extends Binder {
+        public FloatingService getService() {
+            return FloatingService.this;
+        }
+    }
+
+    /**
+     * 设置 WindowManger
+     */
+    public void initWindowManger(){
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         layoutParams = new WindowManager.LayoutParams();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -51,90 +75,43 @@ public class FloatingService extends Service {
         layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
         layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
         layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
-//        layoutParams.x = (int) (ScreenUtil.getDisplayWidth()*0.8);
+        layoutParams.x = 200;
         layoutParams.y = 200;
-
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        removeView();
-    }
-
-//    /**
-//     *  eventBus事件处理 在ui线程执行
-//     * @param event
-//     */
-//    @Subscribe(threadMode = ThreadMode.MAIN)
-//    public void eventBusFunc(String event) {
-//        if (event ==null || event.equals("") || chronometer ==null){
-//            return;
-//        }
-//        if (event.startsWith(UPDATE_TEXT_FLAG)){
-//            long  time  = Long.valueOf( event.replace(UPDATE_TEXT_FLAG+"-",""));
-//            tvStatus.setVisibility(View.GONE);
-//            chronometer.setVisibility(View.VISIBLE);
-//            chronometer.setBase(time);
-//            chronometer.start();
-//        }
-//
-//    }
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        showFloatingWindow(intent);
-        return new FloatingBinder();
-    }
-
-    public class FloatingBinder extends Binder {
-        public FloatingService getService() {
-            return FloatingService.this;
+    /**
+     * 添加悬浮窗布局
+     * @param intent
+     */
+    public void addView(Intent intent){
+        //6.0 以后需要权限，没有权限不处理
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            return;
         }
-    }
-
-
-    private void showFloatingWindow(Intent intent) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)) {
-//            addView(intent);
+        view = LayoutInflater.from(this).inflate(R.layout.view_floating,null);
+        chronometer = view.findViewById(R.id.chronometer);
+        windowManager.addView(view, layoutParams);
+        if (intent !=null){
+            long time = intent.getLongExtra("time",0L);
+            chronometer.setBase(time);
+            chronometer.start();
         }
-    }
-    public   void  removeView(){
-        if (windowManager !=null && view !=null){
-            isStarted = false;
-            windowManager.removeView(view);
-        }
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setClass(FloatingService.this, FloatingViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        view.setOnTouchListener(new FloatingOnTouchListener());
     }
 
-//    public void addView(Intent intent){
-//        view = LayoutInflater.from(this).inflate(R.layout.view_audio_floating,null);
-//        tvStatus = view.findViewById(R.id.tv_status);
-//        chronometer = view.findViewById(R.id.avchat_audio_time);
-//        windowManager.addView(view, layoutParams);
-//        if (intent !=null){
-//            long time = intent.getLongExtra("time",0L);
-//            if (time !=0L){
-//                tvStatus.setVisibility(View.GONE);
-//                chronometer.setVisibility(View.VISIBLE);
-//                chronometer.setBase(time);
-//                chronometer.start();
-//            }
-//        }
-//        view.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                 Intent intent = new Intent();
-//                intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK);
-//                intent.setClass(AudioFloatingService.this, AVChatActivity.class);
-//                startActivity(intent);
-////                removeView();
-//            }
-//        });
-//
-//        view.setOnTouchListener(new FloatingOnTouchListener());
-//    }
-
+    /**
+     * 悬浮窗拖动设置
+     */
     private class FloatingOnTouchListener implements View.OnTouchListener {
         private int x;
         private int y;
